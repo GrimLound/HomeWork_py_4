988a81bf90a38376d408f656724a0ef6c5acf892
Vladislav <vladisbruce@mail.ru>
2023-03-07 16:02:52 +0300

    Решил задачу №2

--- main.py
+++ main.py
@@ -4,24 +4,44 @@
 # которые встречаются в обоих наборах.
 # Пользователь вводит 2 списка.
 # 1 строка - первый список через пробел. 2 строка - второй список через пробел.
+'''
+mol = [int(x) for x in input().split()]
+n = mol[0]
+m = mol[1]
+set_1 = set()
+set_2 = set()
+list_1 = list()
+a = [int(x) for x in input().split()]
+k = set(a)
+for i in k:
+    set_1.add(i)
+b = [int(x) for x in input().split()]
+k1 = set(b)
+for i in k1:
+    set_2.add(i)
+lok = set_1 & set_2
+kool = list(lok)
+kool.sort()
+for i in kool:
+    print(i, end=' ')
+'''
+#--------------------------------------------------------------------------------
 
-# mol = [int(x) for x in input().split()]
-# n = mol[0]
-# m = mol[1]
-# set_1 = set()
-# set_2 = set()
-# list_1 = list()
-# a = [int(x) for x in input().split()]
-# k = set(a)
-# for i in k:
-#     set_1.add(i)
-# b = [int(x) for x in input().split()]
-# k1 = set(b)
-# for i in k1:
-#     set_2.add(i)
-# lok = set_1 & set_2
-# kool = list(lok)
-# kool.sort()
-# for i in kool:
-#     print(i, end=' ')
+# Задача 24:
+# В фермерском хозяйстве в Карелии выращивают чернику.
+# Она растет на круглой грядке, причем кусты высажены только по окружности.
+# Таким образом, у каждого куста есть ровно два соседних.
+# Всего на грядке растет N кустов.
+'''
+n = int(input())
+arr = list()
+for i in range(n):
+    x = int(input())
+    arr.append(x)
+arr_count = list()
+for i in range(len(arr) - 1):
+    arr_count.append(arr[i - 1] + arr[i] + arr[i + 1])
+arr_count.append(arr[-2] + arr[-1] + arr[0])
+print(max(arr_count))
+'''
 #--------------------------------------------------------------------------------
